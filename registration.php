145c1ac9ea0fa6710ae2391c7c7e5f28cf3fb3b4
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-09 22:35:27 +0200

    Add initial files

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YellowCard\JsBasic;
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'YellowCard_JsBasic', __DIR__);
