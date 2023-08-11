723c7de6b7848cbd6df625708d1d3e24a9a9511d
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-11 23:21:28 +0200

    Use paths to import vue

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,5 +3,8 @@ var config = {
         "*": {
             "fadeInElement": "YellowCard_JsBasic/js/fade-in-element"
         }
+    },
+    "paths": {
+        "vue": "https://unpkg.com/vue@3/dist/vue.global"
     }
 };
