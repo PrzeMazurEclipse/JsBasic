4e16f263f07bcef63e4be5ffadf12811e38ac15a
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-15 21:01:12 +0200

    Mixin in alternative way

--- view/frontend/web/js/messages-mixin.js
+++ view/frontend/web/js/messages-mixin.js
@@ -2,7 +2,11 @@ define([], function() {
     'use strict'
 
     return function(originalMessages) {
-        originalMessages.defaults.hideTimeout = 1000;
-        return originalMessages;
+        return originalMessages.extend({
+            defaults: {
+                hideTimeout: 3000,
+                hideSpeed: 100
+            }
+        })
     }
 })
